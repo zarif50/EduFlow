eb5925d5492c5f823a88fe5004142f2dc7be98fb
zarif50 <zarifmahmud250@gmail.com>
2025-01-28 22:34:50 +0600

    Class Management Added

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AcademicYearController;
+use App\Http\Controllers\ClassesController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -19,6 +20,8 @@ Route::group(['prefix'=>'admin'],function(){
     Route::get('dashboard', [AdminController::class,'dashboard'])->name('admin.dashboard');
     Route::get('form', [AdminController::class,'form'])->name('admin.form');
     Route::get('table', [AdminController::class,'table'])->name('admin.table');
+
+    //Academic Year Management
     Route::get('academic-year/create', [AcademicYearController::class,'index'])->name('academic-year.create');
     Route::post('academic-year/store', [AcademicYearController::class,'store'])->name('academic-year.store');
     Route::get('academic-year/read', [AcademicYearController::class,'read'])->name('academic-year.read');
@@ -26,6 +29,14 @@ Route::group(['prefix'=>'admin'],function(){
     Route::get('academic-year/edit/{id}', [AcademicYearController::class,'edit'])->name('academic-year.edit');
     Route::post('academic-year/update', [AcademicYearController::class,'update'])->name('academic-year.update');
     
+
+    //class Management
+    Route::get('class/create', [ClassesController::class,'index'])->name('class.create');
+    Route::post('class/store', [ClassesController::class,'store'])->name('class.store');
+    Route::get('class/read', [ClassesController::class,'read'])->name('class.read');
+    Route::get('class/read/edit/{id}', [ClassesController::class,'edit'])->name('class.edit');
+    Route::post('class/update', [ClassesController::class,'update'])->name('class.update');
+    Route::get('class/delete/{id}', [ClassesController::class,'delete'])->name('class.delete');
   });
 
 
